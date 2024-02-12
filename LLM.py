681c5da64d5c39afdc2196b89421fb944ca997a9
KMnO4-zx <1021385881@qq.com>
2024-02-12 20:52:28 +0800

    update INternLMChat

--- LLM.py
+++ LLM.py
@@ -10,7 +10,15 @@
 from typing import Dict, List, Optional, Tuple, Union
 
 PROMPT_TEMPLATE = dict(
-    RAG_PROMPT_TEMPALTE="""使用以下上下文来回答用户的问题。如果你不知道答案，就说你不知道。总是使用中文回答。
+    RAG_PROMPT_TEMPALTE="""使用以上下文来回答用户的问题。如果你不知道答案，就说你不知道。总是使用中文回答。
+        问题: {question}
+        可参考的上下文：
+        ···
+        {context}
+        ···
+        如果给定的上下文无法让你做出回答，请回答数据库中没有这个内容，你不知道。
+        有用的回答:""",
+    InternLM_PROMPT_TEMPALTE="""先对上下文进行内容总结,再使用上下文来回答用户的问题。如果你不知道答案，就说你不知道。总是使用中文回答。
         问题: {question}
         可参考的上下文：
         ···
@@ -28,6 +36,8 @@ class BaseModel:
     def chat(self, prompt: str, history: List[dict], content: str) -> str:
         pass
 
+    def load_model(self):
+        pass
 
 class OpenAIChat(BaseModel):
     def __init__(self, path: str = '', model: str = "gpt-3.5-turbo-1106") -> None:
@@ -45,3 +55,20 @@ class OpenAIChat(BaseModel):
             temperature=0.1
         )
         return response.choices[0].message.content
+
+class InternLMChat(BaseModel):
+    def __init__(self, path: str = '') -> None:
+        super().__init__(path)
+        self.load_model()
+
+    def chat(self, prompt: str, history: List = [], content: str='') -> str:
+        prompt = PROMPT_TEMPLATE['InternLM_PROMPT_TEMPALTE'].format(question=prompt, context=content)
+        response, history = self.model.chat(self.tokenizer, prompt, history)
+        return response
+
+
+    def load_model(self):
+        import torch
+        from transformers import AutoTokenizer, AutoModelForCausalLM
+        self.tokenizer = AutoTokenizer.from_pretrained(self.path, trust_remote_code=True)
+        self.model = AutoModelForCausalLM.from_pretrained(self.path, torch_dtype=torch.float16, trust_remote_code=True).cuda()
